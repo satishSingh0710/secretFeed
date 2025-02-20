7a1a6d0648f0b0cf1e42cf3a9af7644503297f14
Satish Singh <satishsingh45264209@gmail.com>
2025-02-20 20:12:56 +0530

    no mixed connections

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://0.0.0.0:3001", {
+export const socket = io("https://secretfeed.onrender.com", {
     path: "/socket.io/", // ✅ Ensure it matches the server path
     transports: ["websocket", "polling"], // ✅ Forces WebSocket transport
   });
